f0f097a8aa8d244fbfeaa1da1dd9b980122ea076
Daniele Del Sale <daniele.delsale@gmail.com>
2017-11-30 20:14:56 +0100

    Json Offline Parsing v0.7
    Added row merging
    Virtually works 99% of times

--- parsing.js
+++ parsing.js
@@ -12,8 +12,8 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
     } else {
 
     var json = JSON.parse(data);
-    //console.log(util.inspect(json, false, null));
 
+    // Controlla che la word ci stia (in altezza) nella row
     myThreshold = 5;
     function fits(word, row){
       if(Math.abs(row.average_y-word.y)<myThreshold)
@@ -22,6 +22,7 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
         return false;
     }
 
+    // Salva ogni parola nel giusto formato nel vettore words
     var words = [];
     for(var page of json[0].fullTextAnnotation.pages){
       for(var block of page.blocks){
@@ -39,8 +40,8 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
     }
 
 
-
-    function updateCoord(row) {
+    // Corrects eventual prospective problems making the average of words y coord
+    function updateAverageHeight(row) {
       var sum = 0;
       var count = 0;
       for(var word of row.words) {
@@ -50,8 +51,7 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
       row.average_y = sum / count;
     }
 
-    //righe[] = { words = new Array(), average_y = -1};
-
+    // Inserisce in righe i vari array di parole opportune
     var righe = new Array();
     var found;
     for(var word of words){
@@ -60,7 +60,7 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
         if(fits(word, riga)){
           found = true;
           riga.words.push(word);
-          updateCoord(riga);
+          updateAverageHeight(riga);
           break;
         }
       }
@@ -69,12 +69,54 @@ fs.readFile('myjsonfile3.json', 'utf8', function readFileCallback(err, data){
         tmp.push(word);
         righe.push({
           words: tmp,
-          average_y: word.y
+          average_y: word.y,
+          isValid: false
         });
       }
     }
 
-    console.log(util.inspect(righe, false, null));
+    // Setta isValid = true in caso words contenga un prezzo
+    for(var riga of righe){
+      var i = 0;
+      for(var word of riga.words){
+        if(!isNaN(word.text)){
+          if(i == 2){
+            riga.isValid = true;
+            break;  // da prestare attenzione ai casi con due prezzi per singolo words
+          }
+          else
+            i = 0;
+          i++;
+        }
+        else if((word.text == "." || word.text == ",") && i == 1)
+          i++;
+        else
+          i = 0;
+      }
+    }
+
+    // Accorpamento di nome e relativo prezzo in un singolo item dell'array final
+    var final = new Array();
+    var i = 0;
+    var lastRow = { words: new Array(), isValid: false };
+    for(var riga of righe) {
+      if (!riga.isValid){
+        lastRow = riga;
+        i = 1;
+      }
+      else if(i == 1 && riga.isValid){
+        for(var row of riga.words){
+          lastRow.words.push(row);
+        }
+        final.push(lastRow);
+        console.log("Pusho in final lastrow:\n" + util.inspect(lastRow, false, null)); //TODO: REMOVE THIS
+        i = 0;
+      }
+      else if (riga.isValid) {
+        console.log("Ho già una riga pronta:\n" + util.inspect(riga,false,null)); //TODO: REMOVE THIS
+        final.push(riga);
+      }
+    }
 
   } //else
 });
