c9ce5dc34c306c1fcdf2ec5ac99a4f80fb834849
seicaratteri <pietro.bolcato@studenti.unitn.it>
2017-12-09 21:59:59 +0100

    image resize after upload

---
+++ test.js
@@ -0,0 +1,10 @@
+var Jimp = require("jimp");
+
+// open a file called "lenna.png"
+Jimp.read("test.jpg", function (err, lenna) {
+    if (err) throw err;
+    lenna.resize(256, 256)            // resize
+         .quality(60)                 // set JPEG quality
+         .greyscale()                 // set greyscale
+         .write("lena-small-bw.jpg"); // save
+});
